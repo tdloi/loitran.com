3ce1d7877f217140f4abe820e7ddba7382dc7c1a
Loi Tran <33661324+tdloi@users.noreply.github.com>
2020-10-25 15:09:11 +0700

    Update reset and global css

--- src/constants.ts
+++ src/constants.ts
@@ -1,6 +1,5 @@
 const NOTION_TOKEN = process.env.NOTION_TOKEN || "";
 const BLOG_INDEX_ID = process.env.BLOG_INDEX_ID || "";
 const INDEX_ID = process.env.INDEX_ID || "";
-const API_ENDPOINT = "https://www.notion.so/api/v3";
 
-export { NOTION_TOKEN, BLOG_INDEX_ID, INDEX_ID, API_ENDPOINT };
+export { NOTION_TOKEN, BLOG_INDEX_ID, INDEX_ID };
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,8 @@
 import { AppProps } from "next/app";
 import Head from "next/head";
-// @ts-ignore
-import resetCSS from "minireset.css";
-import "../assets/prism-atom-dark.css";
-import "typeface-raleway";
 import { Layout } from "../components/Layout";
-import { theme } from "../theme";
+import "modern-normalize/modern-normalize.css";
+import "../global.css";
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
@@ -15,25 +12,6 @@ export default function MyApp({ Component, pageProps }: AppProps) {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Component {...pageProps} />
-      <style jsx global>
-        {resetCSS}
-      </style>
-      <style jsx global>{`
-        html,
-        body {
-          padding: 0;
-          margin: 0;
-          background: ${theme.bg};
-          color: ${theme.fg};
-          font-family: Raleway, -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen, Ubuntu,
-            Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
-          font-size: 18px;
-          line-height: 1.75;
-        }
-        .shiki {
-          padding: 1rem 0.5rem;
-        }
-      `}</style>
     </Layout>
   );
 }
