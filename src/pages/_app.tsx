5b6c96af7b7b5256c24215e7a39d8e09bafe7701
Loi Tran <33661324+tdloi@users.noreply.github.com>
2020-10-25 15:28:31 +0700

    Move Layout to _app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,17 +1,37 @@
 import { AppProps } from "next/app";
 import Head from "next/head";
 import "modern-normalize/modern-normalize.css";
-import { Layout } from "@/components/Layout";
+import { Header } from "@/components/Header";
+import { Footer } from "@/components/Footer";
 import "@/global.css";
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <Layout>
+    <div className="layout">
       <Head>
         <title>Loi Tran</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <Component {...pageProps} />
-    </Layout>
+      <Header />
+      <main className="content">
+        <Component {...pageProps} />
+      </main>
+      <Footer />
+      <style jsx>{`
+        .layout {
+          display: grid;
+          grid-template-rows: auto 1fr auto;
+          height: 100vh;
+          max-width: 1080px;
+          margin: 0 auto;
+          padding-left: 2rem;
+          padding-right: 2rem;
+        }
+        .content {
+          padding-top: 2rem;
+          padding-bottom: 2rem;
+        }
+      `}</style>
+    </div>
   );
 }
